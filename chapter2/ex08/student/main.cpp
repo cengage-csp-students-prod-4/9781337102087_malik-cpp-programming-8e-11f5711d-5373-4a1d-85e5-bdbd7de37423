30746491d41694eab9f47cb927819c3b3c4da04a
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-02 23:11:52 +0000

    chapter2/ex08 All Tasks

--- chapter2/ex08/student/main.cpp
+++ chapter2/ex08/student/main.cpp
@@ -1,13 +1,26 @@
-//include statement(s)
-
-//using namespace statement
-
+#include <iostream>
+#include <string>
+using namespace std;
 int main()
 {
-
-    //variable declaration
-    
-    //executable statements
-    
-    //return statement
+    const int SECRET = 11;
+    const double RATE = 12.50;
+int num1, num2, newNum;
+string name;
+double hoursWorked, wages;
+cout << "Enter two integers: ";
+cin >> num1 >> num2;
+cout << "Enter last name: ";
+cin >> name;
+cout << "Enter a decimal number between 0 and 70: ";
+cin >> hoursWorked;
+wages = Rate * hoursWorked;
+cout << "The value of num1 = " << num1 << " and the value of num2 = " << num2 << "." << endl;
+newNum = (num1 * 2) + num2;
+cout << "The value of newNum = " << newNum << endl;
+cout << "Name: " << name << endl;
+cout << "Pay Rate: $" << RATE << endl;
+cout << "Hours Worked: " << hoursWorked << endl;
+cout << "Salery: $ " << wages << endl;
+return 0;
 }
