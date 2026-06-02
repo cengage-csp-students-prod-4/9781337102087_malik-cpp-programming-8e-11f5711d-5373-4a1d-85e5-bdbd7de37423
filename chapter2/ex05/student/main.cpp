1c762ac01eeaacf58e26fe5ac3c82a01428359d3
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-02 06:47:17 +0000

    chapter2/ex05 All Tasks

--- chapter2/ex05/student/main.cpp
+++ chapter2/ex05/student/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-usin namespace std;
+using namespace std;
 
 int main()
 {
-const double PI = 3.14;
-double radius;
-double area;
-double circumference;
+    const double PI = 3.14;
+    double radius;
+    double area;
+    double circumference;
 
- cout << "Enter the radius: ";
+    cout << "Enter the radius: ";
     cin >> radius;
     cout << endl;
+    
     area = PI * radius * radius;
     circumference = 2 * PI * radius;
 
