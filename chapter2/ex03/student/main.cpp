bc93d81a2aebd5f81ba521bfce53fd9518ba03f4
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-02 06:26:29 +0000

    chapter2/ex03 All Tasks

--- chapter2/ex03/student/main.cpp
+++ chapter2/ex03/student/main.cpp
@@ -1,16 +1,21 @@
-//include statement(s)
+#include <iostream>
 
-//using namespace statement
-
+using namespace std;
 
 int main()
 {
 
-//variable declaration
+   //variable declaration
+   int num1, num2, num3, average;
 
+   //executable statements
+   num1 = 125;
+   num2 = 28;
+   num3 = -25;
+   average = 0;
 
-//executable statements
+   average = (num1 + num2 + num3) / 3;
 
-
-//return 0;
+   //return statement
+   return 0;
 }
