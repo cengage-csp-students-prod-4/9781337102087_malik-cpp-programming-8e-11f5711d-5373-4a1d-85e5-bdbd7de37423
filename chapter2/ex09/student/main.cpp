41a33d77bb16e785f72787d57b8ae4f6bd48eeee
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-02 23:32:44 +0000

    chapter2/ex09 All Tasks

--- chapter2/ex09/student/main.cpp
+++ chapter2/ex09/student/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-double score1, score2, score3, score4, score5;
-cin >> score1 >> score2 >> score3 >> score4 >> score5
-double average = (score1 + score2 + score3+ score4 + score5) / 5.0;
-cout << "Your avrage test score is: " << avrage << endl;
-return 0;
+    double score1, score2, score3, score4, score5;
+    cin >> score1 >> score2 >> score3 >> score4 >> score5;
+    double average = (score1 + score2 + score3 + score4 + score5) / 5.0;
+    cout << "Your average test score is: " << average << endl;
+    return 0;
 }
