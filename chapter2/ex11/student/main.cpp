46c5654f33fe7c508c3713d2ba418384c3f3e847
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-02 23:54:18 +0000

    chapter2/ex11 All Tasks

--- chapter2/ex11/student/main.cpp
+++ chapter2/ex11/student/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
 using namespace std;                                   
- 
 int main()
 {
-    // Write your main here
+double tankCapacity;
+double milesPerGallon;
+double maxMiles;
+cout << "Enter the fuel tank copacity (in gallons): ";
+cin >> tankCapacity;
+cout << "Enter the estimated miles per gallon (MPG): ";
+cin >> milesPerGallon;
+maxMiles = tankCapacity * milesPerGallon;
+cout << "The automobile can be driven " << maxMiles << "miles without refueling." << endl;
     return 0;
 }
