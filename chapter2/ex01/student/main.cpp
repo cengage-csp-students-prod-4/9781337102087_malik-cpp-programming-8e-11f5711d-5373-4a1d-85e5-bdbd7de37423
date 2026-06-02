06253b62a801b48b3c90e295c8b9a5871924bd42
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-02 05:55:07 +0000

    Reset Exercise Tue Jun 02 2026 05:55:07 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex01/student/main.cpp
+++ chapter2/ex01/student/main.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-      cout << "****************************************" << endl;
-      cout<<endl;
-      cout<<"*      Programming Assignment 1        *" << endl;
-      cout<<endl;
-      cout<<"*       Author: Angelle Sumrall        *" << endl;
-      cout<<endl;
-      cout << "*     Due Date: Thursday, Jan. 24      *" << endl;
-      cout << endl;
-      cout << "****************************************" << endl;
-
-return 0;
+    // Write your main here
+    return 0;
 }
