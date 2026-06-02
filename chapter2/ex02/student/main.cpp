73230c2d6ee4839579187246444aff5e316d4dec
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-02 06:13:03 +0000

    chapter2/ex02 All Tasks

--- chapter2/ex02/student/main.cpp
+++ chapter2/ex02/student/main.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main() {
-    // Write your main here
+    cout<<"CCCCCCCCC       ++              ++"<<endl;
+    cout<<"CC              ++              ++"<<endl;
+    cout<<"CC        ++++++++++++++  ++++++++++++++"<<endl;
+    cout<<"CC        ++++++++++++++  ++++++++++++++"<<endl;
+    cout<<"CC              ++              ++"<<endl;
+    cout<<"CCCCCCCCC       ++              ++"<<endl;
+    
     return 0;
 }
