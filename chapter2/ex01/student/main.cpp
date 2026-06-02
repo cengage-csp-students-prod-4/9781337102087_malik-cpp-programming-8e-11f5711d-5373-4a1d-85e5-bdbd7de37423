2ace4f9edc758b7a070348139ded1e75621970c6
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-02 06:01:49 +0000

    chapter2/ex01 All Tasks

--- chapter2/ex01/student/main.cpp
+++ chapter2/ex01/student/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    // Write your main here
-    return 0;
+  cout << "****************************************" << endl;
+  cout << endl;
+  cout << "*      Programming Assignment 1        *" << endl;
+  cout << endl;
+  cout << "*       Computer Programming I         *" << endl;
+  cout << endl;
+  cout << "*       Author: Angelle Sumrall        *" << endl;
+  cout << endl;
+  cout << "*     Due Date: Thursday, Jan. 24      *" << endl;
+  cout << endl;
+  cout << "****************************************" << endl;
+
+  return 0;
 }
