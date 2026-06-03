15dd8e54220aff68343c41d27327c1051a00af8f
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 01:18:48 +0000

    chapter2/ex16 All Tasks

--- chapter2/ex16/student/main.cpp
+++ chapter2/ex16/student/main.cpp
@@ -7,6 +7,8 @@ int main() {
     const double COST_PER_LITER = 0.38;
     const double PROFIT_PER_CARTON = 0.27;
     double totalMilk;
+    cout << "Enter the total amount of milk produced: ";
+    cin >> totalMilk;
     int cartonsNeeded = round(totalMilk / CARTON_CAPACITY);
     double productionCost = totalMilk * COST_PER_LITER;
     double totalProfit = cartonsNeeded * PROFIT_PER_CARTON;
