1babfa3acd1ab1ebf2ae053d93820388ce63bde4
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 19:33:56 +0000

    chapter2/ex17 All Tasks

--- chapter2/ex17/student/main.cpp
+++ chapter2/ex17/student/main.cpp
@@ -17,7 +17,7 @@ int main() {
     double totalCost = totalMilk * costPerLiter;
     double totalProfit = cartonsNeeded * profitPerCaron;
     cout << fixed << setprecision(2);
-    cout << "The number of milk cartons needed to hold milk: " cartonsNeeded << endl;
+    cout << "The number of milk cartons needed to hold milk: " << cartonsNeeded << endl;
     cout << "The cost of producing milk: $" << totalCost << endl;
     cout << "The profit for producing milk: $" << totalProfit << endl;
     return 0;
