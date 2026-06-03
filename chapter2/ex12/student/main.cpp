b7abc59ec07cd58c3b0e5140429d1934a06fdb0f
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 00:05:05 +0000

    chapter2/ex12 All Tasks

--- chapter2/ex12/student/main.cpp
+++ chapter2/ex12/student/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-    // Write your main here
+    int total_seconds;
+    cout << "Enter the elapsed time in seconds: ";
+    cin >> total_seconds;
+    int hours = total_seconds / 3600;
+    int remaining_seconds = total_seconds % 3600;
+    int minutes = remaining_seconds / 60;
+    int seconds = remaining_seconds % 60;
+    cout << hours << ":" << minutes << ":" << seconds << endl;
     return 0;
 }
