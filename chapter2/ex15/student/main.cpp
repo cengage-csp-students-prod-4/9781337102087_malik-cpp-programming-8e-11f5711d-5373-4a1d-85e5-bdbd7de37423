cad557f52454fd46696ed1a5c84c2bbfdae72d85
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 00:44:11 +0000

    chapter2/ex15 All Tasks

--- chapter2/ex15/student/main.cpp
+++ chapter2/ex15/student/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
-
 int main() {
-    // Write your main here
+    const double PI = 3.141593;
+    double radius;
+    double price;
+    double area;
+    double pricePerSquareInch;
+    cout << "Enter the radius of the pizza: ";
+    cin >> radius;
+    cout << "Enter the price of the pizza: ";
+    cin >> price;
+    area = PI * radius * radius;
+    pricePerSquareInch = price / area;
+    cout << fixed << setprecision(6);
+    cout << "\nThe price per square inch is" << pricePerSquareInch << endl;
     return 0;
 }
