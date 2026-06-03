fb5040fb4993ecea828dcab13ef18bb373731d1c
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 21:40:18 +0000

    chapter2/ex23 All Tasks

--- chapter2/ex23/student/main.cpp
+++ chapter2/ex23/student/main.cpp
@@ -25,6 +25,8 @@ int main() {
     double totalServiceCharges = buyServiceCharge + sellServiceCharge;
     double netGainOrLoss = totalReceived - totalInvested;
 
+    cout << fixed << showpoint;
+
     cout << "Total amount invested: $" << setprecision(2) << totalInvested << endl;
     cout << "Total amount received: $" << setprecision(2) << totalReceived << endl;
     cout << "Total service charges: $" << setprecision(3) << totalServiceCharges << endl;
