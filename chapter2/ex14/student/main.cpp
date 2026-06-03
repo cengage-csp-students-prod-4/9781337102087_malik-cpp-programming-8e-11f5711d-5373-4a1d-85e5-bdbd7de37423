434b8eb833133ac23be5cc6c7d018c7eb407e445
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 00:31:46 +0000

    chapter2/ex14 All Tasks

--- chapter2/ex14/student/main.cpp
+++ chapter2/ex14/student/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-    // Write your main here
+double manufacturerSizeBG;
+cout << "Enter the hard drive size in BG (as specified by the manufacturer): ";
+cin >> manufacturerSizeBG;
+double totalByters = manufacturerSizeBG * 1000 * 1000 * 1000;
+double actualSizeGB = totalByters / (1024 * 1024 * 1024);
+cout << "The actual storage copacity of the hard drive is: " << actualSizeGB << " GB " << endl;
     return 0;
 }
