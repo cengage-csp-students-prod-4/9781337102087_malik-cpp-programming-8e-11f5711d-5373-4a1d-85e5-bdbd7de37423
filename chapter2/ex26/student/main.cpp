1074bc3305e3054849271be9084557d77eb15c15
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 22:17:51 +0000

    chapter2/ex26 All Tasks

--- chapter2/ex26/student/main.cpp
+++ chapter2/ex26/student/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-    // Write your main here
+    double doorLength, doorWidth;
+    double windowLength, windoWidth;
+    double bookshelfLength, bookshelfWidth;
+    double roomLength, roomWidth, roomHeight;
+
+    cout << "Enter the length and width of the door (in feet): ";
+    cin >> doorLength >> doorWidth;
+    cout << "Enter the length and width of each window (in feet): ";
+    cin >> windowLength >> windoWidth;
+
+    cout << "Enter the length and width of the bookshelf (in feet): ";
+    cin >> bookshelfLength >> bookshelfWidth;
+    cout << "Enter the length, width, and height of the room (in feet): ";
+    cin >> roomLength >> roomWidth >> roomHeight;
+
+    double doorArea = doorLength * doorWidth;
+    double twoWindowsArea = 2 * (windowLength * windoWidth);
+    double bookshelfArea = bookshelfLength * bookshelfWidth;
+    double totalWallArea = 2 * (roomLength * roomHeight) + 2 * (roomWidth * roomHeight);
+    double paintableArea = totalWallArea - (doorArea + twoWindowsArea + bookshelfArea);
+    double gallonsNeeded = paintableArea / 120.0;
+
+    cout << "\n --- Results ---" << endl;
+    cout << "Paintable area: " << paintableArea << " square feet." << endl;
+    cout << "Amount of paint needed: " << gallonsNeeded << " gallons." << endl;
     return 0;
 }
