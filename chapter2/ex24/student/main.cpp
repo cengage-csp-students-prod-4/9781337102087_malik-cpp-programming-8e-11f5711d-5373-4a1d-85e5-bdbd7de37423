0e9d6e4cdc9ddf3319c234e432fc03ef196d735e
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 21:51:36 +0000

    chapter2/ex24 All Tasks

--- chapter2/ex24/student/main.cpp
+++ chapter2/ex24/student/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-    // Write your main here
+    double wireLength;
+    double width;
+    double length;
+
+    cout << "Enter the length of the wwire: ";
+    cin >> wireLength;
+
+    width = wireLength / 5.0;
+    length = 1.5 * width;
+
+    cout << "Width of the picture frame: " << width << endl;
+    cout << "Length of the picture frame: " << length << endl;
     return 0;
 }
