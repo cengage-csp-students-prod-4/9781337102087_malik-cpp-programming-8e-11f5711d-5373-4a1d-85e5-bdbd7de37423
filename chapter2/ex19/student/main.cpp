ce6e2a7896f376d07f52a9eec045fa37002e2b2f
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 20:29:40 +0000

    chapter2/ex19 All Tasks

--- chapter2/ex19/student/main.cpp
+++ chapter2/ex19/student/main.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main() {
-    // Write your main here
+    int quarters, dimes, nickels;
+    cout << "Enter the number of quarters: ";
+    cin >> quarters;
+    
+    cout << "Enter the number of dimes: ";
+    cin >> dimes;
+    
+    cout << "Enter the number of nickels: ";
+    cin >> nickels;
+    int total_pennies = (quarters * 25) + (dimes * 10) + (nickels * 5);
+    cout << total_pennies << endl;
+    
     return 0;
 }
