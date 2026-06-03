e4f5bbeb128ec9d470bb50e9643386deca0a238d
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 21:00:07 +0000

    chapter2/ex21 All Tasks

--- chapter2/ex21/student/main.cpp
+++ chapter2/ex21/student/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-
-using namespace std;
-
+#include <iomanip>
 int main() {
-    // Write your main here
+    const double K = 6.67e-8;
+    double m1, m2, d;
+    std: :cout << "Enter the mass of the first body (M1): ";
+    std: :cin >> m1;
+    std: :cout << "Enter the mass of the second body (M2): ";
+    std: :cin >> m2;
+    std: :cout << "Enter the distance between them (d): ";
+    std: :cin >> d;
+
+    double force = k * (m1 * m2) / (d * d);
+    std: :cout << std: :fixed << std: :setprecision(2);
+        std::cout << "The force between the bodies is: " << force << " dyn" << std::endl;
     return 0;
 }
