01fc4cbecd59c285b119efb3bd510bfdda27ef40
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 22:03:52 +0000

    chapter2/ex25 All Tasks

--- chapter2/ex25/student/main.cpp
+++ chapter2/ex25/student/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-    // Write your main here
+    const double PI = 3.1416;
+    double wireLength;
+    double radius;
+    double area;
+    
+    cout << "Enter the length of the wire: ";
+    cin >> wireLength;
+
+    radius = wireLength / (2 * PI);
+    area = PI * radius * radius;
+
+    cout << "Radius of the circle: " << radius << endl;
+    cout << "Area of the circle: " << area << endl;
     return 0;
 }
