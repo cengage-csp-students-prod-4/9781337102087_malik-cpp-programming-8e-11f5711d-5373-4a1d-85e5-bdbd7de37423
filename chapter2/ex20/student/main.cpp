3d051ec00f8fba2864be9a8a625ced0e8d300d0b
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 20:38:47 +0000

    chapter2/ex20 All Tasks

--- chapter2/ex20/student/main.cpp
+++ chapter2/ex20/student/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-    // Write your main here
+    double purchaseCost;
+    double minMarkup;
+    double maxMarkup;
+    double fixedCommission;
+    double commissionPercentage;
+    cout << "Enter the purchasing cost of the car: $";
+    cin >> purchaseCost;
+
+    cout << "Enter the minimum to be added to the purchasing cost: $";
+    cin >> minMarkup;
+
+    cout << "Enter the maximum to be added to the purchasing cost: $";
+    cin >> maxMarkup;
+
+    cout << "Enter the salesperson's fixed commission: $";
+    cin >> fixedCommission;
+
+    cout << "Enter the percentage of the commission (e.g., 30 for 30%): ";
+    cin >> commissionPercentage;
+    double minSellingPrice = purchaseCost + minMarkup;
+    double maxSellingPrice = purchaseCost + maxMarkup;
+    double minCommission = fixedCommission + ((commissionPercentage / 100.0) * minMarkup);
+    double maxCommission = fixedCommission + ((commissionPercentage / 100.0) * maxMarkup);
+
+    cout << fixed << setprecision(2);
+    cout << "--------------------------------------------------" << endl;
+    cout << "OUTPUT RESULTS:" << endl;
+    cout << "--------------------------------------------------" << endl;
+    cout << "Minimum Selling Price: $" << minSellingPrice << endl;
+    cout << "Maximum Selling Price: $" << maxSellingPrice << endl;
+    cout << "Salesperson's Commission Range: $" << minCommission << " to $" << maxCommission << endl;
+    cout << "--------------------------------------------------" << endl;
+
     return 0;
 }
