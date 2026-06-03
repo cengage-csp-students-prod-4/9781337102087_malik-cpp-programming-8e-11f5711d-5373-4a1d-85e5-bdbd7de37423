fea638a721c63ae2634093461e19f3d3fd1c5dae
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 19:36:57 +0000

    chapter2/ex17 All Tasks

--- chapter2/ex17/student/main.cpp
+++ chapter2/ex17/student/main.cpp
@@ -6,19 +6,19 @@ int main() {
     const double CARTON_CAPACITY = 3.78;
     double totalMilk;
     double costPerLiter;
-    double profitPerCaron;
+    double profitPerCarton;
     cout << "Enter the total amount of milk produced: ";
     cin >> totalMilk;
-    cout << "Enter the cost of producting one liter of milk: ";
+    cout << "Enter the cost of producing one liter of milk: ";
     cin >> costPerLiter
     cout << "Enter the profit on each carton of milk: ";
-    cin >> profitPerCaron;
+    cin >> profitPerCarton;
     int cartonsNeeded = static_cast<int>(round(totalMilk / CARTON_CAPACITY));
     double totalCost = totalMilk * costPerLiter;
-    double totalProfit = cartonsNeeded * profitPerCaron;
+    double totalProfit = cartonsNeeded * profitPerCarton;
     cout << fixed << setprecision(2);
     cout << "The number of milk cartons needed to hold milk: " << cartonsNeeded << endl;
-    cout << "The cost of producing milk: $" << totalCost << endl;
-    cout << "The profit for producing milk: $" << totalProfit << endl;
+    cout << "The cost of producing milk: " << totalCost << endl;
+    cout << "The profit for producing milk: " << totalProfit << endl;
     return 0;
 }
