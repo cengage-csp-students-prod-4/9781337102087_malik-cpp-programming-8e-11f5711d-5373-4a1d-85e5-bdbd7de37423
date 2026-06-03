5e9a0f87438aff023d6ab171aacd16f37b1ba805
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 22:23:12 +0000

    chapter2/ex26 All Tasks

--- chapter2/ex26/student/main.cpp
+++ chapter2/ex26/student/main.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 int main() {
     double doorLength, doorWidth;
-    double windowLength, windoWidth;
+    double window1Length, window1Width;
+    double window2Length, window2Width;
     double bookshelfLength, bookshelfWidth;
     double roomLength, roomWidth, roomHeight;
 
     cout << "Enter the length and width of the door (in feet): ";
     cin >> doorLength >> doorWidth;
     cout << "Enter the length and width of each window (in feet): ";
-    cin >> windowLength >> windoWidth;
+     cin >> window1Length >> window1Width >> window2Length >> window2Width;
 
     cout << "Enter the length and width of the bookshelf (in feet): ";
     cin >> bookshelfLength >> bookshelfWidth;
@@ -18,14 +19,12 @@ int main() {
     cin >> roomLength >> roomWidth >> roomHeight;
 
     double doorArea = doorLength * doorWidth;
-    double twoWindowsArea = 2 * (windowLength * windoWidth);
+    double windowsArea = (window1Length * window1Width) + (window2Length * window2Width);
     double bookshelfArea = bookshelfLength * bookshelfWidth;
     double totalWallArea = 2 * (roomLength * roomHeight) + 2 * (roomWidth * roomHeight);
-    double paintableArea = totalWallArea - (doorArea + twoWindowsArea + bookshelfArea);
+    double paintableArea = totalWallArea - (doorArea + windowsArea + bookshelfArea);
     double gallonsNeeded = paintableArea / 120.0;
 
-    cout << "\n --- Results ---" << endl;
-    cout << "Paintable area: " << paintableArea << " square feet." << endl;
-    cout << "Amount of paint needed: " << gallonsNeeded << " gallons." << endl;
+    cout << fixed << setprecision(3) << gallonsNeeded << endl;
     return 0;
 }
