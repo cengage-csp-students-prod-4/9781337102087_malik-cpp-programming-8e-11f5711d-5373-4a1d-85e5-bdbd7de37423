a83ce2a8acccd5b0378a2d176ccc369d082eeefc
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 01:14:30 +0000

    chapter2/ex16 All Tasks

--- chapter2/ex16/student/main.cpp
+++ chapter2/ex16/student/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-
+#include <cmath>
+#include <iomanip>
 using namespace std;
-
 int main() {
-    // Write your main here
+    const double CARTON_CAPACITY = 3.78;
+    const double COST_PER_LITER = 0.38;
+    const double PROFIT_PER_CARTON = 0.27;
+    double totalMilk;
+    int cartonsNeeded = round(totalMilk / CARTON_CAPACITY);
+    double productionCost = totalMilk * COST_PER_LITER;
+    double totalProfit = cartonsNeeded * PROFIT_PER_CARTON;
+    cout << fixed << setprecision(2);
+    cout << "Number of milk cartons needed: " << cartonsNeeded << endl;
+    cout << "Cost of producing milk: $" << productionCost << endl;
+    cout << "Profit for producing milk: $" << totalProfit << endl;
     return 0;
 }
