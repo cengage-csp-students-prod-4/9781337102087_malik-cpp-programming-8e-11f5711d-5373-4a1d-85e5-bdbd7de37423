83d5e42a69f1371092c7549d37aee2883eebca56
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-03 21:21:18 +0000

    chapter2/ex22 All Tasks

--- chapter2/ex22/student/main.cpp
+++ chapter2/ex22/student/main.cpp
@@ -4,11 +4,10 @@ int main() {
 
     double bag_capacity;
     const double POUNDS_PER_TON = 2205.0;
-    cout << "Enter the amount of rice a bag can hold (in pounds): ";
     cin >> bag_capacity;
 
     double bags_needed = POUNDS_PER_TON / bag_capacity;
-    
-    cout << 'Number of bags needed: ' << bags_needed << endl;
+
+    cout << bags_needed << endl;
     return 0;
 }
