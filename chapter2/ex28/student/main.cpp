b0b6899a3dcbf1092d5d405683f85da6b3ec49e7
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-04 20:12:48 +0000

    chapter2/ex28 All Tasks

--- chapter2/ex28/student/main.cpp
+++ chapter2/ex28/student/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
-
 int main() {
-    // Write your main here
+    int numStudents;
+    double caloriesPerStudent;
+    double caloriesPerPoundNuts;
+    double totalCaloriesNeeded;
+    double caloriesPerPoundFruit;
+    double poundsEach;
+
+    cout << "Enter the number of students: ";
+    cin >> numStudents;
+    cout << "Enter the calories required per student: ";
+    cin >> caloriesPerStudent;
+    cout << "Enter the calroeis per pound of nuts: ";
+    cin >> caloriesPerPoundNuts;
+
+    totalCaloriesNeeded = numStudents * caloriesPerStudent;
+    caloriesPerPoundFruit = caloriesPerPoundNuts / 0.70;
+
+    cout << fixed << setprecision(2);
+    cout << "Amount of nuts needed: " << poundsEach << "lbs" << endl;
+    cout << "Amount of dried fruit needed: " << poundsEach << "lbs" << endl;
     return 0;
 }
