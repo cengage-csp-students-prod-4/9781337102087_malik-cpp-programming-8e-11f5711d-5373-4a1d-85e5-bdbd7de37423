dc8b00f83df3ba570474c81c809a186d353e2c3f
isabell20062006-rgb <isabell20062006@gmail.com>
2026-06-04 20:16:08 +0000

    chapter2/ex28 All Tasks

--- chapter2/ex28/student/main.cpp
+++ chapter2/ex28/student/main.cpp
@@ -20,7 +20,6 @@ int main() {
     caloriesPerPoundFruit = caloriesPerPoundNuts / 0.70;
     poundsEach = totalCaloriesNeeded / (caloriesPerPoundNuts + caloriesPerPoundFruit);
 
-    cout << fixed << setprecision(3);
     cout << "Amount of nuts needed: " << poundsEach << "lbs" << endl;
     cout << "Amount of dried fruit needed: " << poundsEach << "lbs" << endl;
     return 0;
